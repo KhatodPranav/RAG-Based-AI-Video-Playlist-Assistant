ca9858f865115c31b728e0636a749d1f1788fc72
KhatodPranav <289pranavk@gmail.com>
2026-09-12 23:28:53 +0530

    saving dataframe to joblib and creating prompt for LLM

---
+++ getting_response.py
@@ -0,0 +1,33 @@
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+from read_chunks import create_embedding
+import joblib
+
+file=joblib.load("embeddings.joblib")
+df=file
+
+question=input("Enter Your Question: ")
+question_embeddings=create_embedding(question)[0]
+
+# print(np.vstack(df["embedding"].values))
+# print(np.vstack(df["embedding"].shape))
+
+similarity=cosine_similarity(np.vstack(df["embedding"]),[question_embeddings]).flatten()
+# print(similarity)
+
+top_results=5
+max_indx=similarity.argsort()[::-1][0:top_results]
+# print(max_indx)
+
+top_idx_data=df.loc[max_indx]
+# print(top_idx_data[["chunk_id","video_number","text"]])
+# print(top_idx_data.keys())
+
+prompt=f"""Here are video subtitle chunks containing Video Number,start time in seconds,end time in seconds, text for that time:
+{top_idx_data[["video_number","start","end","text"]].to_json()}
+------------------------------
+Users Question: {question}
+User asked this question related to the video chunk, you have to answer where and how much content is taught in which video (in which video and at what time stamp) and guide the user to go to that particular video. If user asks any unrelated question, tell him you can only answer questions related to selected videos."""
+
+with open("prompt.txt","w") as f:
+    f.write(prompt)
--- read_chunks.py
+++ read_chunks.py
@@ -2,6 +2,7 @@ import requests
 import os
 import json
 import pandas as pd
+import joblib
 
 def create_embedding(text_list):
     r=requests.post("http://localhost:11434/api/embed",json={
@@ -11,27 +12,29 @@ def create_embedding(text_list):
     embedding=r.json()["embeddings"]
     return embedding
 
-jsons=os.listdir("jsons")
-l=[]
-chunk_id=0
+if __name__ == "__main__":
+    jsons=os.listdir("jsons")
+    l=[]
+    chunk_id=0
 
-for json_file in jsons:
-    with open(f"jsons/{json_file}") as f:
-        content=json.load(f)
-    texts=[chunk.get("text","") for chunk in content]
-    embeddings = []
-    batch_size = 80
-    for i in range(0, len(texts), batch_size):
-        batch=texts[i:i +batch_size]
-        print(f"Embedding {i} to {i+len(batch)-1}")
-        batch_embeddings=create_embedding(batch)
-        embeddings.extend(batch_embeddings)
+    for json_file in jsons:
+        with open(f"jsons/{json_file}") as f:
+            content=json.load(f)
+        texts=[chunk.get("text","") for chunk in content]
+        embeddings = []
+        batch_size = 80
+        for i in range(0, len(texts), batch_size):
+            batch=texts[i:i +batch_size]
+            print(f"Embedding {i} to {i+len(batch)-1}")
+            batch_embeddings=create_embedding(batch)
+            embeddings.extend(batch_embeddings)
 
-    for i,chunk in enumerate(content):
-        chunk["chunk_id"]=chunk_id
-        chunk["embedding"]=embeddings[i]
-        chunk_id +=1
-        l.append(chunk)
+        for i,chunk in enumerate(content):
+            chunk["chunk_id"]=chunk_id
+            chunk["embedding"]=embeddings[i]
+            chunk_id +=1
+            l.append(chunk)
 
-df=pd.DataFrame.from_records(l)
-print(df)
+    df=pd.DataFrame.from_records(l)
+
+    joblib.dump(df,"embeddings.joblib")
